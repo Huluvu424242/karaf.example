7468f03af236ccfd34a3650a48ce501bec9580c5
Thomas Schubert <funthomas424242@gmail.com>
2014-10-05 21:55:13 +0200

    initial check in - project setup via maven archetype

---
+++ Karaf-JPA-Command/src/main/java/com/github/funthomas424242/App.java
@@ -0,0 +1,13 @@
+package com.github.funthomas424242;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
